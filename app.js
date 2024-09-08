216bd5821c8b94abd7b531d8138ac1ecba1fa1c1
shanaldahanayake <shanaldahanayake99@gmail.com>
2024-09-08 23:01:38 +0530

    second commit

--- app.js
+++ app.js
@@ -3,32 +3,29 @@ const search = document.querySelector("#submit");
 
 const apiKey = "91f501fe6824414c84e85533240509";
 
-const getWeatherDetails=(cityName)=>{
-    
-    const urlForecast=`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=5&aqi=no&alerts=no`;
-
-    fetch(urlForecast).then(res=>res.json()).then(data=>{
-        console.log(data);
-    }).catch(() => {
-        alert("An Error Occured in Forecast Data");
-    });
-}
-
 const getCityCoordinate = () => {
     const cityName = city.value.trim();
-    if (!cityName) return;
-    console.log(cityName)
-
+    if (!cityName) {
+        alert("Enter a City");
+    }
+    
     const urlCurrent = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
 
     fetch(urlCurrent).then(res => res.json()).then(data => {
     
+        if (data.location.name==null) {
+            alert("Not a Valid City");
+            return;
+        }
+
         document.querySelector("#entered-city").innerHTML="City : "+data.location.name;
         document.querySelector("#temp").innerHTML="Temparature : "+data.current.temp_c+"°C";
         document.querySelector("#wind").innerHTML="Wind : "+data.current.wind_mph+"mph";
         document.querySelector("#humadity").innerHTML="Humadity : "+data.current.humidity+"%";
         document.querySelector("#image").src=data.current.condition.icon;
         document.querySelector("#status").innerHTML=data.current.condition.text;
+    }).catch(error => {
+        console.error("Error fetching the weather data:", error);
     });
 }
 search.addEventListener('click', getCityCoordinate)
